7fc7e5f341d46c8cf181738f68bf9f275296932a
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-09-16 22:34:16 +0900

    published

--- js/jquery.slick.extension.js
+++ js/jquery.slick.extension.js
@@ -135,110 +135,154 @@ try {
 			/**
 			 * @name slickExtension
 			 * @since 2018-08-02
-			 * @param {object} option {lowIE : boolean, autoArrow : element || jQueryElement, playArrow : element || jQueryElement, pauseArrow : element || jQueryElement, pauseOnArrowClick : boolean, playText : string, pauseText : string}
+			 * @param {object} option {lowIE : boolean, autoArrow : element || jQueryElement, playArrow : element || jQueryElement, pauseArrow : element || jQueryElement, pauseOnArrowClick : boolean, play : string, pause : string, current : element || jQueryElement, total : element || jQueryElement, customState : function}
 			 * @return {jqueryElement}
 			 */
 			$.fn.slick = function(option) {
-				var $thisFirst = this.first(),
-					isObject = _getType(option) === 'object',
+				var isObject = _getType(option) === 'object',
 					isString = typeof option === 'string';
 
-				//요소이면서 매개변수가 객체거나 문자거나 없을때
-				if(_isElement($thisFirst) && (isObject || isString || typeof option === 'undefined')) {
-					//슬릭을 사용하면서 매개변수가 문자가 아닐때
-					if($thisFirst.hasClass('slick-initialized') && !isString) {
-						$thisFirst.slick('unslick');
-					}
+				return this.each(function(index, element) {
+					var $element = $(element);
 
-					//슬릭이 함수일때
-					if(typeof _slick === 'function') {
-						//ie7, ie8 브라우저이면서 객체이면서 option.lowIE가 거짓일때
-						if(_isLowIE && isObject && !option.lowIE) {
-							delete option.responsive;
+					//요소이면서 매개변수가 객체거나 문자거나 없을때
+					if(_isElement($element) && (isObject || isString || typeof option === 'undefined')) {
+						//슬릭을 사용하면서 매개변수가 문자가 아닐때
+						if($element.hasClass('slick-initialized') && !isString) {
+							$element.slick('unslick');
 						}
 
-						//슬릭적용
-						_slick.call($thisFirst, option);
-					
-						//객체일때
-						if(isObject) {
-							//자동버튼, 재생버튼, 정지버튼, 이전버튼, 다음버튼 요소정의
-							option.autoArrow = $(option.autoArrow);
-							option.playArrow = $(option.playArrow);
-							option.pauseArrow = $(option.pauseArrow);
-							option.prevArrow = $(option.prevArrow);
-							option.nextArrow = $(option.nextArrow);
-							
-							var autuArrowHTML = option.autoArrow.html() || '';
-
-							//문자가 아닐때
-							if(typeof option.playText !== 'string') {
-								option.playText = '';
+						//슬릭이 함수일때
+						if(typeof _slick === 'function') {
+							//ie6, 7, 8 브라우저이면서 객체이면서 option.lowIE가 거짓일때
+							if(_isLowIE && isObject && !option.lowIE) {
+								delete option.responsive;
 							}
-
-							//문자가 아닐때
-							if(typeof option.pauseText !== 'string') {
-								option.pauseText = '';
-							}
-
-							//일시정지 상태일때
-							if($thisFirst[0].slick.paused) {
-								$thisFirst.slick('slickPause');
-								option.autoArrow.addClass('active').text(option.playText);
-							}else{
-								$thisFirst.slick('slickPlay');
-								option.autoArrow.removeClass('active').text(option.pauseText);
-							}
-
-							//자동버튼
-							option.autoArrow.off('click.slickExtension').on('click.slickExtension', function(event) {
-								var $this = $(this);
-
-								//일시정지 상태일때
-								if($thisFirst[0].slick.paused) {
-									$thisFirst.slick('slickPlay');
-									$this.removeClass('active').text(option.pauseText);
-								}else{
-									$thisFirst.slick('slickPause');
-									$this.addClass('active').text(option.playText);
-								}
-
-								event.preventDefault();
-							});
 							
-							//재생버튼
-							option.playArrow.off('click.slickExtension').on('click.slickExtension', function(event) {
-								option.autoArrow.removeClass('active').text(option.pauseText);
-								$thisFirst.slick('slickPlay');
-								event.preventDefault();
-							});
-							
-							//일시정지 버튼
-							option.pauseArrow.off('click.slickExtension').on('click.slickExtension', function(event) {
-								option.autoArrow.addClass('active').text(option.playText);
-								$thisFirst.slick('slickPause');
-								event.preventDefault();
-							});
-							
-							//option.pauseOnArrowClick가 참일때
-							if(option.pauseOnArrowClick) {
-								//이전, 재생버튼
-								option.prevArrow.add(option.nextArrow).off('click.slickExtension').on('click.slickExtension', function(event) {
-									$thisFirst.slick('slickPause');
-									option.autoArrow.addClass('active').text(option.playText);
+							//객체일때
+							if(isObject) {
+								option.$total = $(option.total);
+								option.$current = $(option.current);
+
+								//셋팅되었을때, 슬라이드가 넘어갔을때
+								$element.on('init.slickExtension beforeChange.slickExtension', function(event, slick, currentSlide, nextSlide) {
+									var current = nextSlide,
+										total = slick.slideCount;
+									
+									//다음 슬라이드가 있을때
+									if(current) {
+										current++;
+									}else{
+										//슬라이드 갯수가 있을때
+										if(total) {
+											current = 1;
+										}else{
+											current = 0;
+										}
+									}
+
+									//함수일때
+									if(typeof option.customState === 'function') {
+										var customState = option.customState({
+											current : current,
+											total : total
+										});
+
+										//객체가 아닐때
+										if(_getType(customState) !== 'object') {
+											customState = {
+												current : current,
+												total : total
+											};
+										}
+
+										current = customState.current || current;
+										total = customState.total || total;
+									}
+
+									option.$current.text(current);
+									option.$total.text(total);
 								});
 							}
 
-							//파괴되었을때
-							$thisFirst.on('destroy.slickExtension', function(event, slick) {
-								option.autoArrow.add(option.playArrow).add(option.pauseArrow).add(option.prevArrow).add(option.nextArrow).off('click.slickExtension');
-							});
+							//슬릭적용
+							_slick.call($element, option);
+
+							//객체일때
+							if(isObject) {
+								//자동버튼, 재생버튼, 정지버튼, 이전버튼, 다음버튼 요소정의
+								option.$autoArrow = $(option.autoArrow);
+								option.$playArrow = $(option.playArrow);
+								option.$pauseArrow = $(option.pauseArrow);
+								option.$prevArrow = $(option.prevArrow);
+								option.$nextArrow = $(option.nextArrow);
+
+								//문자가 아닐때
+								if(typeof option.play !== 'string') {
+									option.play = '';
+								}
+
+								//문자가 아닐때
+								if(typeof option.pause !== 'string') {
+									option.pause = '';
+								}
+
+								//일시정지 상태일때
+								if($element[0].slick.paused) {
+									$element.slick('slickPause');
+									option.$autoArrow.addClass('active').text(option.play);
+								}else{
+									$element.slick('slickPlay');
+									option.$autoArrow.removeClass('active').text(option.pause);
+								}
+
+								//자동버튼
+								option.$autoArrow.off('click.slickExtension').on('click.slickExtension', function(event) {
+									var $this = $(this);
+
+									//일시정지 상태일때
+									if($element[0].slick.paused) {
+										$element.slick('slickPlay');
+										$this.removeClass('active').text(option.pause);
+									}else{
+										$element.slick('slickPause');
+										$this.addClass('active').text(option.play);
+									}
+
+									event.preventDefault();
+								});
+								
+								//재생버튼
+								option.$playArrow.off('click.slickExtension').on('click.slickExtension', function(event) {
+									option.$autoArrow.removeClass('active').text(option.pause);
+									$element.slick('slickPlay');
+									event.preventDefault();
+								});
+								
+								//일시정지 버튼
+								option.$pauseArrow.off('click.slickExtension').on('click.slickExtension', function(event) {
+									option.$autoArrow.addClass('active').text(option.play);
+									$element.slick('slickPause');
+									event.preventDefault();
+								});
+								
+								//option.pauseOnArrowClick이 참일때
+								if(option.pauseOnArrowClick) {
+									//이전, 재생버튼
+									option.$prevArrow.add(option.$nextArrow).off('click.slickExtension').on('click.slickExtension', function(event) {
+										$element.slick('slickPause');
+										option.$autoArrow.addClass('active').text(option.play);
+									});
+								}
+
+								//파괴되었을때
+								$element.on('destroy.slickExtension', function(event, slick) {
+									option.$autoArrow.add(option.$playArrow).add(option.$pauseArrow).add(option.$prevArrow).add(option.$nextArrow).off('click.slickExtension');
+								});
+							}
 						}
 					}
-				}
-
-				//요소 반환
-				return $thisFirst;
+				});
 			};
 		}else{
 			throw '제이쿼리가 없습니다.';
