4d9065620596c270149134db08e693dc3a2762f2
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-10-12 13:39:48 +0900

    published

--- js/slick.extensions.js
+++ js/slick.extensions.js
@@ -248,7 +248,7 @@ try {
 						 * @since 2018-08-02
 						 */
 						function getSlickOptions() {
-							return $.extend(slick.breakpointSettings[slick.activeBreakpoint], slick.settings) || {};
+							return $.extend(slick.breakpointSettings[slick.activeBreakpoint], slick.options) || {};
 						}
 
 						//파괴되었을때
